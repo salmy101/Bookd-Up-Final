c1bf388cde99d98022bf5cbc956a553eba712a6e
Timi Alabi <alabitimi65@gmail.com>
2022-08-12 23:06:23 +0000

    restructured the search page

--- react-front-end/src/routes/search2.js
+++ react-front-end/src/routes/search2.js
@@ -1,45 +1,92 @@
 import React, { useState } from "react";
 import { cleanUpSearchResults, getBooksBySearch } from "../helpers/booksAPI";
 import { addToShelf } from "../helpers/database";
+import "./search2.scss";
 
 export default function Search() {
-
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   const [result, setResult] = useState([]);
 
   const startSearch = () => {
-    getBooksBySearch(search, page)
-    .then(res => {
+    getBooksBySearch(search, page).then((res) => {
       setResult(cleanUpSearchResults(res.data.items));
-    })
-  }
+    });
+  };
 
   const getResults = (results) => {
     return results.map((result, index) => {
       return (
-        <div key={index} onClick={() => addToShelf(1, result.industryIdentifiers[0].identifier)} style={{border: '1px solid black', width: '200px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <div>{<img src={(result.imageLinks && result.imageLinks.thumbnail) || "images/no-book-thumbnail.png"} alt="Current Book"/>}</div>
-        <p><b>{result.title}</b></p>
-        <p>{result.publishedDate.split('-')[0]}</p>
-        <p>{result.industryIdentifiers[0].identifier}</p>
-        <p>{`by ${result.authors[0]}`}</p>
-      </div>
+        <div
+          className="book-container"
+          key={index}
+          onClick={() =>
+            addToShelf(1, result.industryIdentifiers[0].identifier)
+          }
+        >
+          <div className="image-box">
+            {
+              <img
+                className="book-image"
+                src={
+                  (result.imageLinks && result.imageLinks.thumbnail) ||
+                  "images/no-book-thumbnail.png"
+                }
+                alt="Current Book"
+              />
+            }
+          </div>
+
+          <div className="book-details-box">
+            <p className="book-title">
+              <b>{result.title}</b>
+            </p>
+            <p className="book-year">
+              Year of Publication: {result.publishedDate.split("-")[0]}
+            </p>
+            <p className="book-isbn">
+              ISBN: {result.industryIdentifiers[0].identifier}
+            </p>
+            <p className="book-author">Author: {` ${result.authors[0]}`}</p>
+            <button className="more-details"> See more </button>
+          </div>
+        </div>
       );
     });
-  }
+  };
 
   return (
-    <div style={{marginTop: '100px', marginLeft: '50px', marginBottom: '100px'}}>
-      <h2>This is the Search!</h2>
-      <form autoComplete='off' onSubmit={event => event.preventDefault()}>
-        <input placeholder='Search' value={search} onChange={event => setSearch(event.target.value)}/>
-        <button onClick={startSearch}>Search</button>
-        <button onClick={() => setPage(page + 1)}>Page Up</button>
-        <button onClick={() => (page > 1) ? setPage(page - 1) : null}>Page Down</button>
-        <p>Page: {page}</p>
+    <div className="search-container">
+      <h2>Search Bookd-Up</h2>
+      <form
+        className="search-form"
+        autoComplete="off"
+        onSubmit={(event) => event.preventDefault()}
+      >
+        <input
+          placeholder="Search"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <div className="button-box">
+          <button className="search-buttons" onClick={startSearch}>
+            Search
+          </button>
+        </div>
       </form>
-      {result.length > 0 && getResults(result)}
+
+      <div className="container">{result.length > 0 && getResults(result)}</div>
+      <div className="pagination-btn">
+        <button
+          className="search-buttons"
+          onClick={() => (page > 1 ? setPage(page - 1) : null)}
+        >
+          Prev
+        </button>
+        <button className="search-buttons" onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
     </div>
   );
-};
+}
--- react-front-end/src/routes/signup.js
+++ react-front-end/src/routes/signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Axios from "axios"
+import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./signup.scss";
 
@@ -9,18 +9,19 @@ export default function SignUp() {
   const navigate = useNavigate();
 
   const register = () => {
-    Axios.post('/api/users', {
-      email: emailAtReg, 
+    Axios.post("/api/users", {
+      email: emailAtReg,
       password: passwordAtReg,
-      first_name:"Salma",
-      last_name: "Ibrahim"
-    }).then((res) => {
-      navigate("/");
-    }).catch((err) => {
-      console.log("ERROR: ====", err);
+      first_name: "Salma",
+      last_name: "Ibrahim",
     })
-  }
-
+      .then((res) => {
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("ERROR: ====", err);
+      });
+  };
 
   return (
     <section className="sign-up-section">
@@ -29,32 +30,35 @@ export default function SignUp() {
       </div>
 
       <div className="form-box">
-        <form onSubmit={(event) => event.preventDefault()} >
+        <form onSubmit={(event) => event.preventDefault()}>
           <div className="form-container">
-            <input 
-            type="text" 
-            name="email" 
-            placeholder="Email"
-            onChange={(e) => {
-              setEmailAtReg(e.target.value);
-            }}
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={(e) => {
+                setEmailAtReg(e.target.value);
+              }}
             ></input>
           </div>
 
           <div className="form-container">
-            <input 
-            type="text" 
-            name="password" 
-            placeholder="Password"
-            onChange={(e) => {
-              setPasswordAtReg(e.target.value);
-            }}
+            <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPasswordAtReg(e.target.value);
+              }}
             ></input>
           </div>
         </form>
 
-        <button onClick={register} className="signup-btn"> Sign Up</button>
+        <button onClick={register} className="signup-btn">
+          {" "}
+          Sign Up
+        </button>
       </div>
     </section>
   );
-};
+}
