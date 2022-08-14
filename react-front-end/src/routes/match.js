398ecca36b1a567f6a945d3675b17fda2d9be602
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-14 11:08:22 -0500

    created matchbook

--- react-front-end/src/routes/match.js
+++ react-front-end/src/routes/match.js
@@ -1,17 +1,89 @@
-import React from "react";
-import { Canvas } from '@react-three/fiber';
-
+import React, { useEffect, useState } from "react";
+import Book3D from '../components/Book3D';
+import { getBookBySelfLink, getBooksLinksBySubject } from "../helpers/booksAPI";
+import { genres } from "../helpers/genres";
+import './match.scss';
 
 export default function Match() {
 
+  const [genre, setGenre] = useState();
+  const [results, setResults] = useState();
+  const [book, setBook] = useState();
+  const [bookIndex, setBookIndex] = useState();
+
+  useEffect(() => {
+    console.log('Genre: ', genre);
+  }, [genre]);
+
+  useEffect(() => {
+    console.log('Book: ', book);
+  }, [book])
+
+  useEffect(() => {
+    console.log('Book Index: ', bookIndex);
+  }, [bookIndex]);
+
+  useEffect(() => {
+    console.log('Results: ', results);
+  }, [results]);
+
+  const getNewBook = () => {
+    setBookIndex(prev => prev + 1);
+    if (results) {
+      getBookBySelfLink(results[bookIndex].selfLink)
+      .then(res => setBook(res.data.volumeInfo));
+    }
+  }
+
+  const handlePickGenre = (category) => {
+    setGenre(category);
+    setBookIndex(0);
+    getBooksLinksBySubject(category)
+    .then(res => {
+      setResults(res.data.items);
+      getNewBook();
+    });
+  }
+
+  const getGenres = (genres) => {
+    return genres.map((category, index) => {
+      return (
+        <div
+          key={index}
+          className={`genre ${genre === category && 'selected'}`}
+          onClick={() => handlePickGenre(category)}
+        >
+          {category}
+        </div>
+      );
+    });
+  }
 
   return (
-    <Canvas style={{marginTop: 100}}>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
+    <div className="main">
+      <div className="genres-container">
+        Pick a genre:
+        {getGenres(Object.keys(genres))}
+      </div>
+      <div className="matchbook-container">
+        <div className="canvas-container">
+          {/* <Book3D coverImage={(book && book.imageLinks && book.imageLinks.thumbnail) || 'images/no-book-thumbnail.png'} /> */}
+          <Book3D coverImage={'images/no-book-thumbnail.png'} pages={(book && book.pageCount) || 300} />
+        </div>
+          <header className="basic-info-container">
+            <div className="basic-info-title">{book && book.title}</div>
+            <div>📖 {(book && book.pageCount && `${book.pageCount} pages`) || 'No pages'}</div>
+            <div>🗓 {(book && book.publishedDate && book.publishedDate.split('-')[0]) || 'No year'}</div>
+            <div>⭐️ {(book && book.averageRating) || 'No reviews'} {book && book.ratingsCount && `(${book.ratingsCount} reviews)`}</div>
+            <div>👤 {(book && book.authors && book.authors[0]) || 'No author'}</div>
+            <div style={{marginTop: 10}}>Description</div>
+            <div className="basic-info-description" dangerouslySetInnerHTML={{__html: (book && book.description) || 'No description'}}></div>
+          </header>
+          <footer className="icons-container">
+            <div className="skip" onClick={() => getNewBook()}></div>
+            <div className="like"></div>
+          </footer>
+      </div>
+    </div>
   );
-
-};
+}
