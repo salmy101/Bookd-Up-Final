59bf1dd1d8e9cc5c747fd62b9579f9e9f6ce3a9b
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-14 11:06:47 -0500

    added basic fiction genres

---
+++ react-front-end/src/helpers/genres.js
@@ -0,0 +1,14 @@
+const genres = {
+  'Adventure': [],
+  'Classics': [],
+  'Crime': [],
+  'Disaster': [],
+  'Dystopian': [],
+  'Fantasy': ['General', 'Action & Adventure', 'Dark Fantasy', 'Military', 'Paranormal', 'Romance'],
+  'Mystery & Detective': ['General', 'Amateur Sleuth', 'Historical', 'Private Investigators'],
+  'Romance': ['General', 'Action & Adventure', 'Billionaires', 'Contemporary', 'Fantasy', 'Holiday', 'Medical', 'Romantic Comedy', 'Time Travel', 'Workplace'],
+  'Science Fiction': ['General', 'Action & Adventure', 'Crime & Mystery', 'Cyberpunk', 'Military', 'Space Exploration', 'Steampunk', 'Time Travel'],
+  'Thrillers': ['General', 'Crime', 'Espionage', 'Legal', 'Psychological', 'Supernatural', 'Suspense']
+}
+
+export { genres };
