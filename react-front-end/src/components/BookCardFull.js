c085f9233e2646677cdfd835068e7e1a2c5d00ce
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-15 17:29:55 -0500

    added ability to add book to shelf

--- react-front-end/src/components/BookCardFull.js
+++ react-front-end/src/components/BookCardFull.js
@@ -1,27 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { getBookBySelfLink } from "../helpers/booksAPI";
+import { addToShelf } from "../helpers/database";
 import "./BookCardFull.scss";
-
 
 export default function BookCardFull(props) {
 
-  const { selfLink } = props;
+  const { selfLink, setBookSelfLink } = props;
   const [book, setBook] = useState();
-  const [show, setShow] = useState('');
+  const [showCard, setShowCard] = useState('');
+  const [showShelves, setShowShelves] = useState('');
 
   useEffect(() => {
     getBookBySelfLink(selfLink)
     .then(res => {
       setBook(res.data.volumeInfo);
-      setShow('--show');
+      setShowCard('--show');
+      setShowShelves('');
     });
   }, []);
 
+  const closeBookCardFull = () => {
+    setShowCard('');
+    setShowShelves('');
+    setBookSelfLink(null);
+  }
+
+  const handlePickShelf = (shelf) => {
+    addToShelf('1', book.industryIdentifiers[0].identifier, shelf);
+    setShowShelves('');
+  }
+
   return (
-    <div className={`book-full${show}`}>
+    <div className={`book-full${showCard}`}>
       <div className="book-full-overlay">
         <div className="book-full-container">
-          <img className="book-full-close-btn" onClick={() => setShow('')} src="images/close-full-book.png" alt="Close Button"/>
+          <img className="book-full-close-btn" onClick={() => closeBookCardFull()} src="images/close-full-book.png" alt="Close Button"/>
           <div className="book-full-left">
             {book && book.title && <div style={{fontWeight: 900, fontSize: "x-large"}}>{book.title}</div>}
             {book && book.subtitle && <div style={{fontWeight: 700}}>{book.subtitle}</div>}
@@ -45,14 +58,19 @@ export default function BookCardFull(props) {
                 book.imageLinks.large ||
                 book.imageLinks.medium ||
                 book.imageLinks.small ||
-                book.imageLinks.thumbnail||
+                book.imageLinks.thumbnail ||
                 "images/no-book-thumbnail.png")) ||
                 "images/no-book-thumbnail.png"}
               alt="Full Book Cover"
             />
             <div style={{fontWeight: 800}}>Add to:</div>
-            <div className="add-to-btns">My Shelf</div>
-            <div className="add-to-btns">Bookclub's Currently Reading</div>
+            <div className={`shelf-btns-container${showShelves}`}>
+              <div className="shelf-btn" onClick={() => handlePickShelf('current_reads')}>Currently Reading</div>
+              <div className="shelf-btn" onClick={() => handlePickShelf('want_to_reads')}>Want To Read</div>
+              <div className="shelf-btn" onClick={() => handlePickShelf('have_reads')}>Finished Reading</div>
+            </div>
+            <div className="add-to-btn" onClick={() => setShowShelves(showShelves === '' ? '--show' : '')}>My Shelf</div>
+            <div className="add-to-btn">Bookclub's Currently Reading</div>
           </div>
 
         </div>
