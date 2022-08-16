1b296a8e6284bf0037f06a3ea76424abb8f491bd
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-16 13:01:00 -0500

    added cors anywhere as cors proxy

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyparser = require("body-parser");
 const { Pool } = require('pg');
 const morgan = require('morgan');
+const corsProxy = require('cors-anywhere');
 const PORT = 8080;
 
 const dbParams = {
@@ -37,4 +38,13 @@ app.get('/api/data', (req, res) => res.json({
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
+});
+
+// Creating and starting CORS Anywhere server
+corsProxy.createServer({
+  originWhitelist: [],
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(8081, '0.0.0.0', () => {
+  console.log('Running CORS Anywhere on ' + '0.0.0.0' + ':' + 8081);
 });
